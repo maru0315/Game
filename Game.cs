a9f4b7c2eaab0533e580789e12eae52a082870c9
maru0315 <maru0315.000@gmail.com>
2015-07-28 13:38:04 +0900

    追加します

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    //敵の攻撃力・定義（ランダム）____
+    public class Dice
+    {
+        public int Roll()
+        {
+            Random r = new Random();
+            return r.Next(999, 4999);
+        }
+    }
+}
